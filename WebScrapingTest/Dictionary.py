a981f9a0236836b89168a78ac1e1c2aa9c46253c
TheBurntFish <Tianfishburn@gmail.com>
2023-06-02 00:52:42 -0700

    Add project files.

--- WebScrapingTest/Dictionary.py
+++ WebScrapingTest/Dictionary.py
@@ -9,7 +9,7 @@ def chooseWord():
     inp = input("Choose an English word\n")
     if(inp == ""):
         print("Selecting random word...")
-        myFile = open('C:\\Users\\tianf\\Downloads\\Programming projects\\words_dictionary.json')
+        myFile = open('words_dictionary.json')
         myDictionary = json.loads(myFile.read())
         myKeys = list(myDictionary.keys())
         rand = random.randint(0, len(myKeys))
